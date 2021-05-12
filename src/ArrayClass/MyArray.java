d88e205755225b7694a209297391f642ff3689a7
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-05-12 00:59:59 -0400

    fixed issues on array with deleting, and index out of range errors

--- src/ArrayClass/MyArray.java
+++ src/ArrayClass/MyArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MyArray<T> {
     private T[] arrayData; // its set to generic array data this way anything can be stored
-    private int length; // what is length of the array.
+    public int length; // what is length of the array.
     private int maxItems; // how many max items will be allowed here
 
     public MyArray(int maxItems) {
@@ -13,28 +13,23 @@ public class MyArray<T> {
         arrayData = (T[]) new Object[maxItems];
     }
     public T getIndex(int index){
-        return arrayData[index];
+        return arrayData[index-1];
     }
     public void Push(T item){
         arrayData[length]=item;
         this.length++;
-        System.out.println("Element in the array is "+ Arrays.toString(arrayData));
-
-        System.out.println("item on the array is "+length);
     }
     public void Pop(){
         var lastItem = this.arrayData[this.length-1]; // this will hold the value for the item
         this.arrayData[this.length-1]= (T)new Object(); // this way object will be cased to
         this.length --;
-        System.out.println("the last Item was deleted "+ lastItem);
-        System.out.println("The Length of the array is now " + length);
         System.out.println("Element in the array is "+ Arrays.toString(arrayData));
 
     }
 
     public void printItems(){
         System.out.println("Items in the array");
-        for (int i = 0; i <maxItems ; i++) {
+        for (int i = 0; i <length ; i++) {
             System.out.println(arrayData[i]);
         }
         System.out.println("");
@@ -42,14 +37,14 @@ public class MyArray<T> {
 
 
 
+
     public void Delete(int index){
-        var item = this.arrayData[index];
-    }
-    public void ShiftItems(int index){
-        for (int i = index; i <index-1 ; i++) {
+         index-=1;
+        for (int i = index; i <this.length-1 ; i++) {
+
             this.arrayData[i] = this.arrayData[i+1]; // shift it
         }
-        this.arrayData[this.length-1] =(T)new Object();
+
         this.length--;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,18 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var test = new MyArray<String>(10);
-        System.out.println(test.getIndex(0));
-        test.Push("hello");
-        test.Push("hi");
 
-        var intTest = new MyArray<Integer>(10);
-        intTest.Push(10);
-        intTest.Push(20);
-        intTest.Push(30);
-        intTest.Push(40);
-        intTest.ShiftItems(1);
-        intTest.printItems();
 
 
     }
